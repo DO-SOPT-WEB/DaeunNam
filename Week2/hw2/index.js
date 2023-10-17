5c1056a30cbf3810f7cc360e5d7d3964bbaefd3f
namdaeun <nde40345@gmail.com>
2023-10-18 02:17:50 +0900

    feat: checkbox 필터링 기능 구현

--- Week2/hw2/index.js
+++ Week2/hw2/index.js
@@ -7,44 +7,66 @@ const HISTORY_LIST = [
   { category: "식비", content: "강남역 깍뚝에서 삼쏘", amount: -30000 }
 ];
 
-function updateBalanceAndHistory() {
-    let totalIncome = 0;
-    let totalCost = 0;
-    
-    const listScroll = document.querySelector(".scrollArea");
-    listScroll.innerHTML = ""; 
-      
-    for (const history of HISTORY_LIST) {
-      if (history.amount > 0) {
-        totalIncome += history.amount;
-        priceClass = "income_price"
-      } else {
-        totalCost -= history.amount;
-        priceClass = "cost_price"
-      }
-    
-      const listView = document.createElement("article");
-      listView.className = "listView";
-  
-      const ulElement = document.createElement("ul");
-      ulElement.innerHTML = `
-        <li class="category">${history.category}</li>
-        <li class="content">${history.content}</li>
-        <li class="${priceClass}">${history.amount}</li>
-        <li><input type="button" class="delete_btn"></li>
-      `;
-    
-      listView.appendChild(ulElement);
-      listScroll.appendChild(listView);
-    }
-    
-    const totalElement = document.querySelector(".total");
-    const incomeElement = document.querySelector(".income");
-    const costElement = document.querySelector(".cost");
-    
-    balance = INIT_BALANCE + totalIncome - totalCost;
-    totalElement.textContent = balance;
-    incomeElement.textContent = totalIncome;
-    costElement.textContent = totalCost;
-  }
+document.getElementById("income").addEventListener("change", updateBalanceAndHistory);
+document.getElementById("cost").addEventListener("change", updateBalanceAndHistory);
 updateBalanceAndHistory();
+
+function updateBalanceAndHistory () {
+  const showIncome = document.getElementById("income").checked;
+  const showCost = document.getElementById("cost").checked;
+
+  const filteredHistories = filterHistories(showIncome, showCost);
+
+  let totalIncome = 0;
+  let totalCost = 0;
+  const listScroll = document.querySelector(".scrollArea");
+  listScroll.innerHTML = "";
+
+  const totalElement = document.querySelector(".total");
+  const incomeElement = document.querySelector(".income");
+  const costElement = document.querySelector(".cost");
+
+  for (const history of filteredHistories) {
+    if (history.amount > 0) {
+      totalIncome += history.amount;
+      priceClass = "income_price";
+    } else {
+      totalCost -= history.amount;
+      priceClass = "cost_price";
+    }
+    listScroll.appendChild(createListView(history, priceClass));
+  }
+
+  balance = INIT_BALANCE + totalIncome - totalCost;
+  totalElement.textContent = balance;
+  incomeElement.textContent = totalIncome;
+  costElement.textContent = totalCost;
+}
+
+function createListView (history, priceClass) {
+  const listView = document.createElement("article");
+  listView.className = "listView";
+  
+  const ulElement = document.createElement("ul");
+  ulElement.innerHTML = `
+    <li class="category">${history.category}</li>
+    <li class="content">${history.content}</li>
+    <li class="${priceClass}">${history.amount}</li>
+    <li><input type="button" class="delete_btn"></li>
+  `;
+  
+  listView.appendChild(ulElement);
+  return listView;
+}
+
+function filterHistories (showIncome, showCost) {
+  const filteredHistories = [];
+
+  for (const history of HISTORY_LIST) {
+    if ((showIncome && history.amount > 0) || (showCost && history.amount < 0)) {
+      filteredHistories.push(history);
+    }
+  }
+
+  return filteredHistories;
+}
