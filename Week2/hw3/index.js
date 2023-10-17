66769ff9d85c1167e4e2cc1b88dbbea52fe7a73d
namdaeun <nde40345@gmail.com>
2023-10-18 00:59:37 +0900

    remove: 기존의 hw3 hw4 디렉토리 제거

--- Week2/hw3/index.js
+++
@@ -1,9 +0,0 @@
-let scrollTopBtn = document.getElementById("scrollTop");
-
-document.addEventListener('scroll', () => {
-    const topStartY = scrollTopBtn.getBoundingClientRect().height;
-    const topEndY = scrollTopBtn.getBoundingClientRect().bottom;
-    const topBtnHeight = topEndY - topStartY;
-
-    scrollTopBtn.style.opacity = 1 - topBtnHeight / window.scrollY;
-});
