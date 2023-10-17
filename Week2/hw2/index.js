62869bbfdae3eff132eaa9e46606e24c04eba7c6
namdaeun <nde40345@gmail.com>
2023-10-18 02:55:20 +0900

    feat: 리스트 delete 기능

--- Week2/hw2/index.js
+++ Week2/hw2/index.js
@@ -6,44 +6,41 @@ const HISTORY_LIST = [
   { category: "월급", content: "이번달 월급", amount: 300000 },
   { category: "식비", content: "강남역 깍뚝에서 삼쏘", amount: -30000 }
 ];
+let totalIncome = 0;
+let totalCost = 0; 
 
-document.getElementById("income").addEventListener("change", updateBalanceAndHistory);
-document.getElementById("cost").addEventListener("change", updateBalanceAndHistory);
-updateBalanceAndHistory();
+document.getElementById("income").addEventListener("change", updateHistory);
+document.getElementById("cost").addEventListener("change", updateHistory);
+updateHistory();
 
-function updateBalanceAndHistory () {
+function updateHistory() {
   const showIncome = document.getElementById("income").checked;
   const showCost = document.getElementById("cost").checked;
-
   const filteredHistories = filterHistories(showIncome, showCost);
 
-  let totalIncome = 0;
-  let totalCost = 0;
-  const listScroll = document.querySelector(".scrollArea");
+  let listScroll = document.querySelector(".scrollArea");
   listScroll.innerHTML = "";
 
-  const totalElement = document.querySelector(".total");
-  const incomeElement = document.querySelector(".income");
-  const costElement = document.querySelector(".cost");
-
   for (const history of filteredHistories) {
+    let priceClass = "income_price";
     if (history.amount > 0) {
-      totalIncome += history.amount;
       priceClass = "income_price";
     } else {
-      totalCost -= history.amount;
       priceClass = "cost_price";
     }
-    listScroll.appendChild(createListView(history, priceClass));
-  }
+    const listView = createListView(history, priceClass);
 
-  balance = INIT_BALANCE + totalIncome - totalCost;
-  totalElement.textContent = balance;
-  incomeElement.textContent = totalIncome;
-  costElement.textContent = totalCost;
+    const deleteBtn = listView.querySelector(".delete_btn");
+    deleteBtn.addEventListener("click", function() {
+      deleteHistories(listView, history);
+    });
+
+    listScroll.appendChild(listView);
+  }
+  updateBalance();
 }
 
-function createListView (history, priceClass) {
+function createListView(history, priceClass) {
   const listView = document.createElement("article");
   listView.className = "listView";
   
@@ -54,12 +51,11 @@ function createListView (history, priceClass) {
     <li class="${priceClass}">${history.amount}</li>
     <li><input type="button" class="delete_btn"></li>
   `;
-  
   listView.appendChild(ulElement);
   return listView;
 }
 
-function filterHistories (showIncome, showCost) {
+function filterHistories(showIncome, showCost) {
   const filteredHistories = [];
 
   for (const history of HISTORY_LIST) {
@@ -67,6 +63,40 @@ function filterHistories (showIncome, showCost) {
       filteredHistories.push(history);
     }
   }
-
   return filteredHistories;
 }
+
+function deleteHistories(listView, history) {
+  const listScroll = document.querySelector(".scrollArea");
+  const historyIndex = HISTORY_LIST.indexOf(history);
+  listScroll.removeChild(listView);
+
+  if (historyIndex !== -1) {
+    HISTORY_LIST.splice(historyIndex, 1);
+  }  
+  updateBalance();
+  updateHistory();
+}
+
+function updateBalance() {
+  const totalElement = document.querySelector(".total");
+  const incomeElement = document.querySelector(".income");
+  const costElement = document.querySelector(".cost");
+
+  totalIncome = 0;
+  totalCost = 0;
+
+  for (const history of HISTORY_LIST) {
+    if (history.amount > 0) {
+      totalIncome += history.amount;
+    } else {
+      totalCost -= history.amount;
+    }
+  }
+
+  balance = INIT_BALANCE + totalIncome - totalCost;
+  totalElement.textContent = balance;
+  incomeElement.textContent = totalIncome;
+  costElement.textContent = totalCost;
+}
+
