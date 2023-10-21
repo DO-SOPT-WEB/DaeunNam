2eb6eb76f4feff19f7ef073f7ddcccee83054449
namdaeun <nde40345@gmail.com>
2023-10-22 05:57:47 +0900

    refactor: showModal, openModal, closeModal, deleteBtnHandler 함수 추가

--- Week2/hw2/index.js
+++ Week2/hw2/index.js
@@ -1,68 +1,57 @@
 const INIT_BALANCE = 0;
 let balance = INIT_BALANCE;
 const HISTORY_LIST = [
-  { category: "식비", content: "버거킹 문정역점", amount: -10800 },
+  { category: "식비", content: "버거킹 문정역점", amount: -10000 },
   { category: "취미", content: "포토그레이강남2호점", amount: -5000 },
   { category: "월급", content: "이번달 월급", amount: 300000 },
   { category: "식비", content: "강남역 깍뚝에서 삼쏘", amount: -30000 }
 ];
 let totalIncome = 0;
 let totalCost = 0;
+let priceClass = "income_price";
 let financeType = "income";
 
 document.getElementById("income").addEventListener("change", updateHistory);
 document.getElementById("cost").addEventListener("change", updateHistory);
 document.querySelector('.save_btn').addEventListener('click', addHistoryToList);
-document.querySelectorAll('input[name="finance"]').forEach(function(radio) {
-  radio.addEventListener("change", function() {
-    if (document.getElementById("radio-income").checked) {
-      financeType = "income";
-    } else if (document.getElementById("radio-cost").checked) {
-      financeType = "cost";
-    }
-    filterCategoryList();
-  });
-});
-
 updateHistory();
+checkRadioFinanceType();
+openModal();
+closeModal();
 
 function updateHistory () {
-  const showIncome = document.getElementById("income").checked;
-  const showCost = document.getElementById("cost").checked;
-  const filteredHistories = filterHistories(showIncome, showCost);
-  const addButton = document.querySelector(".add_btn");
-  const modal = document.querySelector(".modal")
-  const closeButton = document.querySelector(".cancel_btn");
-
-  const openModal = () => {
-    modal.classList.remove("hidden");
-  }
-  addButton.addEventListener("click", openModal);
-  const closeModal = () => {
-    modal.classList.add("hidden");
-  }
-  closeButton.addEventListener("click", closeModal);
-
+  const incomeChecked = document.getElementById("income").checked;
+  const costChecked = document.getElementById("cost").checked;
+  const filteredHistories = filterHistories(incomeChecked, costChecked);
   let listScroll = document.querySelector(".scrollArea");
   listScroll.innerHTML = "";
 
   for (const history of filteredHistories) {
-    let priceClass = "income_price";
-    if (history.amount > 0) {
-      priceClass = "income_price";
-    } else {
-      priceClass = "cost_price";
-    }
+    priceClass = history.amount > 0 ? "income_price" : "cost_price";
     const listView = createListView(history, priceClass);
-
-    const deleteBtn = listView.querySelector(".delete_btn");
-    deleteBtn.addEventListener("click", function() {
-      deleteHistory(listView, history);
-    });
+    deleteBtnHandler(listView, history);
     listScroll.appendChild(listView);
   }
   updateBalance();
   filterCategoryList();
+}
+
+function filterHistories (incomeChecked, costChecked) {
+  const filteredHistories = [];
+
+  for (const history of HISTORY_LIST) {
+    if ((history.amount > 0 && incomeChecked) || ( history.amount < 0 && costChecked)) {
+      filteredHistories.push(history);
+    }
+  }
+  return filteredHistories;
+}
+
+function deleteBtnHandler(listView, history) {
+  const deleteBtn = listView.querySelector(".delete_btn");
+  deleteBtn.addEventListener("click", () => {
+    deleteHistory(listView, history);
+  });
 }
 
 function createListView (history, priceClass) {
@@ -78,17 +67,6 @@ function createListView (history, priceClass) {
   `;
   listView.appendChild(ulElement);
   return listView;
-}
-
-function filterHistories (showIncome, showCost) {
-  const filteredHistories = [];
-
-  for (const history of HISTORY_LIST) {
-    if ((showIncome && history.amount > 0) || (showCost && history.amount < 0)) {
-      filteredHistories.push(history);
-    }
-  }
-  return filteredHistories;
 }
 
 function deleteHistory (listView, history) {
@@ -128,6 +106,19 @@ function calculateBalance () {
   costElement.textContent = totalCost;
 }
 
+function checkRadioFinanceType () {
+  document.querySelectorAll('input[name="finance"]').forEach(function(radio) {
+    radio.addEventListener("change", function() {
+      if (document.getElementById("radio-income").checked) {
+        financeType = "income";
+      } else if (document.getElementById("radio-cost").checked) {
+        financeType = "cost";
+      }
+      filterCategoryList();
+    });
+  });
+};
+
 function filterCategoryList() {
   const categoryList = document.querySelector(".category-list");
   categoryList.innerHTML = "";
@@ -150,6 +141,22 @@ function filterCategoryList() {
   });
 }
 
+function showModal() {
+  const modal = document.querySelector(".modal");
+  modal.classList.remove("hidden");
+}
+
+function openModal() {
+  const addButton = document.querySelector(".add_btn");
+  addButton.addEventListener("click", showModal);
+}
+
+function closeModal () {
+  const modal = document.querySelector(".modal");
+  const closeButton = document.querySelector(".cancel_btn");
+  modal.classList.add("hidden");
+  closeButton.addEventListener("click", closeModal);
+}
 
 function addHistoryToList() {
   const financeType = document.querySelector('input[name="finance"]:checked').id;
@@ -175,6 +182,4 @@ function amountShouldbeNumber (amount, newHistory) {
   }
 }
 
-function displayModal() {
-  modal.classList.toggle("hidden");
-}
+
