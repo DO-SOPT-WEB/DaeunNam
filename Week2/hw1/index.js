ddb03466a29fc20f210343167d38b2c08c08ecd7
namdaeun <nde40345@gmail.com>
2023-11-02 15:54:26 +0900

    feat: 이미지 호버 기능 JS로 재구현

--- Week2/hw1/index.js
+++ Week2/hw1/index.js
@@ -1,4 +1,22 @@
 let scrollTopBtn = document.querySelector(".scroll_top");
+const images = document.querySelectorAll('.london_image');
+
+images.forEach(imageContainer => {
+    const image = imageContainer.querySelector('img');
+    const caption = imageContainer.querySelector('.caption');
+
+    image.addEventListener('mouseover', (event) => {
+        image.style.filter = 'brightness(50%)';
+        image.style.transform = 'translateY(-5px)';
+        caption.style.opacity = 1;
+    });
+
+    image.addEventListener('mouseout', (event) => {
+        image.style.filter = 'brightness(100%)';
+        image.style.transform = 'translateY(5px)';
+        caption.style.opacity = 1;
+    });
+});
 
 document.addEventListener('scroll', () => {
     const topStartY = scrollTopBtn.getBoundingClientRect().height;
