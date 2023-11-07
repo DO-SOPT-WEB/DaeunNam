82321230203a2f081758b35bc68d4180dd77efc6
namdaeun <nde40345@gmail.com>
2023-11-08 01:31:26 +0900

    feat: 선택된 옵션으로 메뉴 필터링하여 하나만 반환

--- Week3/Lunch-Recommendation/src/CustomizePage.jsx
+++ Week3/Lunch-Recommendation/src/CustomizePage.jsx
@@ -35,6 +35,21 @@ function CustomizePage() {
 
     const handleRecommendMenu = () => {
         setStep(step + 1);
+        const filteredMenus = MENU_LIST.filter((menu) => {
+            return (
+                (options.cuisine === '' || menu.cuisine === options.cuisine) &&
+                (options.mainIngredient === '' || menu.mainIngredient === options.mainIngredient) &&
+                (options.soup === '' || menu.soup === options.soup)
+            );
+        });
+
+        if (filteredMenus.length > 0) {
+            // 필터링된 메뉴 배열 중 랜덤 인덱스 하나 반환
+            const randomIndex = Math.floor(Math.random() * filteredMenus.length);
+            setRecommendedMenu(filteredMenus[randomIndex].name);
+        } else {
+            setRecommendedMenu('텅 😅');
+        }
     };
 
     return (
