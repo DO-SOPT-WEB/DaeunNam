41710b9fac50b0625082defcea5f13d04627a510
namdaeun <nde40345@gmail.com>
2023-11-07 23:39:14 +0900

    feat: RandomPage 카운트다운 후 랜덤 메뉴 출력

--- Week3/Lunch-Recommendation/src/RandomPage.jsx
+++ Week3/Lunch-Recommendation/src/RandomPage.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import RetryButton from './components/RetryButton';
 
-function RandomPage() {
+const RandomPage = () => {
+    const menus = ['뼈해장국', '스테이크', '초밥', '파스타', '버거', '짜장면', '카레', '떡볶이'];
+
+    const [time, setTime] = useState(3);
+    const [recommendedMenu, setRecommendedMenu] = useState(null);
+
+    useEffect(() => {
+        time > 0 && setTimeout(() => setTime(time - 1), 1000);
+    }, [time]);
+
+    useEffect(() => {
+        if (time <= 0) {
+            const randomIndex = Math.floor(Math.random() * menus.length);
+            setRecommendedMenu(menus[randomIndex]);
+        }
+    }, [time, menus]);
+
     return (
-        <h1>RandomPage</h1>
-    )
-}
+        <div>
+            {time > 0 ? (
+                <h2>{time}</h2>
+            ) : (
+                <h3>오늘의 추천 메뉴: {recommendedMenu}</h3>
+            )}
+        </div>
+    );
+};
 
-export default RandomPage
+export default RandomPage;
