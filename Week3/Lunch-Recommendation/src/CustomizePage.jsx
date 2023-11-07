5930ed9c7ef3bad9e015b6c6823ba2d90dceda2d
namdaeun <nde40345@gmail.com>
2023-11-08 01:18:03 +0900

    docs: MENU_LIST 추가

--- Week3/Lunch-Recommendation/src/CustomizePage.jsx
+++ Week3/Lunch-Recommendation/src/CustomizePage.jsx
@@ -1,6 +1,16 @@
 import { useState } from "react";
 
 const MENU_LIST = [
+    { name: "초밥", cuisine: "일식", mainIngredient: "밥", soup: "국물 없음" },
+    { name: "짬뽕", cuisine: "중식", mainIngredient: "면", soup: "국물" },
+    { name: "불고기", cuisine: "한식", mainIngredient: "고기/해물", soup: "국물 없음" },
+    { name: "뼈해장국", cuisine: "한식", mainIngredient: "고기/해물", soup: "국물" },
+    { name: "마라탕", cuisine: "중식", mainIngredient: "고기/해물", soup: "국물" },
+    { name: "간장계란밥", cuisine: "한식", mainIngredient: "밥", soup: "국물 없음" },
+    { name: "우동", cuisine: "일식", mainIngredient: "면", soup: "국물" },
+    { name: "라멘", cuisine: "일식", mainIngredient: "면", soup: "국물" },
+    { name: "돈까스", cuisine: "일식", mainIngredient: "고기/해물", soup: "국물 없음" },
+    { name: "비빔밥", cuisine: "한식", mainIngredient: "밥", soup: "국물 없음" }
 ]
 
 function CustomizePage() {
