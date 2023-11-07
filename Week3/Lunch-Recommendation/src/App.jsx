8642e1d2fb4e3f67f805aaa5e4daf89d55703d26
namdaeun <nde40345@gmail.com>
2023-11-07 20:27:31 +0900

    docs: App.jsx의 내용 InitScreen.jsx로 옮기고 그에 맞게 코드 수정

--- Week3/Lunch-Recommendation/src/App.jsx
+++ Week3/Lunch-Recommendation/src/App.jsx
@@ -1,64 +1,14 @@
-import { useState } from 'react';
 import InitScreen from './components/InitScreen';
-import styled from 'styled-components';
-import CustomizeButton from './components/CustomizeButton';
-import RandomButton from './components/RandomButton';
-import CustomizePage from './CustomizePage';
-import RandomPage from './RandomPage';
 
 const App = () => {
-  const [selectedOption, setSelectedOption] = useState(null);
-  const [recommendationStart, setRecommendationStart] = useState(false);
-
-  const handleStart = () => {
-    setRecommendationStart(true);
-  };
-
-  const handleCustomize = () => {
-    setSelectedOption('customize');
-  };
-
-  const handleRandom = () => {
-    setSelectedOption('random');
-  };
-
-  const handleCancel = () => {
-    setRecommendationStart(false);
-    setSelectedOption(null);
-  };
 
   return (
-    <MainView>
-      {selectedOption ? (
-        <>
-          <InitScreen />
-          <SelectedOption>{selectedOption}</SelectedOption>
-          {((selectedOption === 'customize') && !recommendationStart ||
-            (selectedOption === 'random') && !recommendationStart) ? (
-            <>
-              <button onClick={handleStart} type='button'>Start!</button>
-              <button onClick={handleCancel} type='button'>돌아가기</button>
-            </>
-          ) : null}
-          {recommendationStart && selectedOption === 'customize' ? <CustomizePage /> : null}
-          {recommendationStart && selectedOption === 'random' ? <RandomPage /> : null}
-        </>
-      ) : (
-        <>
-          <InitScreen />
-          <CustomizeButton handleCustomize={handleCustomize} />
-          <RandomButton handleRandom={handleRandom} />
-        </>
-      )}
-    </MainView>
+    <>
+      <h1>😋 점메추 😋</h1>
+      <InitScreen />
+    </>
   );
 }
 
 export default App;
 
-const MainView = styled.div`
-  text-align: center;
-`
-
-const SelectedOption = styled.h2`
-`
--- Week3/Lunch-Recommendation/src/components/InitScreen.jsx
+++ Week3/Lunch-Recommendation/src/components/InitScreen.jsx
@@ -1,9 +1,51 @@
+import { useState } from 'react';
+import CustomizeButton from './CustomizeButton';
+import RandomButton from './RandomButton';
+import CustomizePage from '../CustomizePage';
+import RandomPage from '../RandomPage';
+
 function InitScreen() {
+    const [selectedOption, setSelectedOption] = useState(null);
+    const [recommendationStart, setRecommendationStart] = useState(false);
+
+    const handleStart = () => {
+        setRecommendationStart(true);
+    };
+
+    const handleCustomize = () => {
+        setSelectedOption('customize');
+    };
+
+    const handleRandom = () => {
+        setSelectedOption('random');
+    };
+
+    const handleCancel = () => {
+        setRecommendationStart(false);
+        setSelectedOption(null);
+    };
 
     return (
         <>
-            <h1>😋 점메추 😋</h1>
             <h2>원하는 추천 방식을 골라줘!</h2>
+            {selectedOption ? (
+                <>
+                    <h3>{selectedOption}</h3>
+                    {((selectedOption === 'customize' || selectedOption === 'random') && !recommendationStart) ? (
+                        <>
+                            <button onClick={handleStart} type='button'>Start!</button>
+                            <button onClick={handleCancel} type='button'>돌아가기</button>
+                        </>
+                    ) : null}
+                    {recommendationStart && selectedOption === 'customize' ? <CustomizePage /> : null}
+                    {recommendationStart && selectedOption === 'random' ? <RandomPage /> : null}
+                </>
+            ) : (
+                <>
+                    <CustomizeButton handleCustomize={handleCustomize} />
+                    <RandomButton handleRandom={handleRandom} />
+                </>
+            )}
         </>
     );
 }
