b6bce6f19d39ccfc0dc0782ef25239279d7ddb89
namdaeun <nde40345@gmail.com>
2023-11-10 18:38:37 +0900

    docs: style.js 임포트

--- Week3/Lunch-Recommendation/src/components/Contents/RecommendTypeButton.jsx
+++ Week3/Lunch-Recommendation/src/components/Contents/RecommendTypeButton.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import handleCustom from './SelectRecommendType';
-import handleRandom from './SelectRecommendType';
+import * as S from './style.js';
 
 function RecommendTypeButton({ handleRandom, handleCustomize }) {
     return (
         <>
-            <button type='button' onClick={handleCustomize}>골라 먹을래</button>
-            <button type="button" onClick={handleRandom}>아무거나 먹을래</button>
+            <S.button type='button' onClick={handleCustomize}>골라 먹을래</S.button>
+            <S.button type="button" onClick={handleRandom}>아무거나 먹을래</S.button>
         </>
     )
 }
