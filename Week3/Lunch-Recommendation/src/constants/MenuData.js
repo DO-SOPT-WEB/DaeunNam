353f1ce08393f492f174f98a3b7b183c1c160ef4
namdaeun <nde40345@gmail.com>
2023-11-10 19:01:06 +0900

    docs: 메뉴 추가

--- Week3/Lunch-Recommendation/src/constants/MenuData.js
+++ Week3/Lunch-Recommendation/src/constants/MenuData.js
@@ -8,7 +8,16 @@ const MENU_LIST = [
     { name: "우동", cuisine: "일식", mainIngredient: "면", soup: "국물" },
     { name: "라멘", cuisine: "일식", mainIngredient: "면", soup: "국물" },
     { name: "돈까스", cuisine: "일식", mainIngredient: "고기/해물", soup: "국물 없음" },
-    { name: "비빔밥", cuisine: "한식", mainIngredient: "밥", soup: "국물 없음" }
+    { name: "비빔밥", cuisine: "한식", mainIngredient: "밥", soup: "국물 없음" },
+    { name: "오꼬노미야끼", cuisine: "일식", mainIngredient: "고기/해물", soup: "국물 없음" },
+    { name: "훠궈", cuisine: "중식", mainIngredient: "고기/해물", soup: "국물" },
+    { name: "가츠동", cuisine: "일식", mainIngredient: "밥", soup: "국물 없음" },
+    { name: "장어덮밥", cuisine: "일식", mainIngredient: "고기/해물", soup: "국물 없음" },
+    { name: "깐풍기", cuisine: "중식", mainIngredient: "고기/해물", soup: "국물 없음" },
+    { name: "떡갈비", cuisine: "한식", mainIngredient: "고기/해물", soup: "국물 없음" },
+    { name: "탕수육", cuisine: "중식", mainIngredient: "고기/해물", soup: "국물 없음" },
+    { name: "냉면", cuisine: "한식", mainIngredient: "면", soup: "국물" },
+    { name: "쫄순", cuisine: "한식", mainIngredient: "면", soup: "국물" }
 ];
 
 export default MENU_LIST;
