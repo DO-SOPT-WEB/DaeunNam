85aa43f323fac3ce1922ae8509f5de17951566ea
namdaeun <nde40345@gmail.com>
2023-11-14 20:46:57 +0900

    Docs: add MyPage

---
+++ Week4/LoginAndSignup/src/pages/MyPage/MyPage.jsx
@@ -0,0 +1,16 @@
+const MyPage = () => {
+    return (
+        <>
+            <h3>MY PAGE</h3>
+            <div>
+                ID :
+            </div>
+            <div>
+                닉네임 :
+            </div>
+            <button type="button">로그아웃</button>
+        </>
+    );
+};
+
+export default MyPage;
