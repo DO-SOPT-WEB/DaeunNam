e5f379fe44713b7faba3ca42903c80ed1f08ceb8
namdaeun <nde40345@gmail.com>
2023-11-17 05:52:18 +0900

    Feat: 로그인 데이터 가져와서 화면에 띄우기

--- Week4/LoginAndSignup/src/pages/MyPage/MyPage.jsx
+++ Week4/LoginAndSignup/src/pages/MyPage/MyPage.jsx
@@ -1,21 +1,42 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from "axios";
 import * as S from './style';
+import { useState } from 'react';
 
 const MyPage = () => {
+    const { userId } = useParams();
     const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [nickname, setNickname] = useState("");
 
     const moveLoginPage = () => {
         navigate(`/login`);
     };
 
+    const getLoginData = async () => {
+        try {
+            axios.get(`${import.meta.env.VITE_BASE_URL}/api/v1/members/${userId}`, {
+                userId: userId,
+            }).then((response) => {
+                setUsername(response.data.username);
+                setNickname(response.data.nickname);
+                console.log("✨🔥성공🔥✨");
+
+            })
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    getLoginData();
+
     return (
         <S.Container>
             <S.PageTitle>MY PAGE</S.PageTitle>
             <S.TextArea>
-                ID :
+                ID : {username}
             </S.TextArea>
             <S.TextArea>
-                닉네임 :
+                닉네임 : {nickname}
             </S.TextArea>
             <S.Button type="button" onClick={moveLoginPage}>로그아웃</S.Button>
         </S.Container>
