8244f43f40b2a00afdb438e59dd78a01e35775d0
namdaeun <nde40345@gmail.com>
2023-11-17 05:45:42 +0900

    Feat: 회원가입 버튼 누르면 Signup으로 이동

--- Week4/LoginAndSignup/src/pages/Login/Login.jsx
+++ Week4/LoginAndSignup/src/pages/Login/Login.jsx
@@ -1,14 +1,47 @@
+import { useState } from 'react';
+import * as S from './style';
+import { useNavigate } from 'react-router-dom';
+
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    const saveUsername = event => {
+        setUsername(event.target.value);
+    };
+
+    const savePassword = event => {
+        setPassword(event.target.value);
+    };
+
+    const moveSignupPage = () => {
+        navigate(`/signup`);
+    };
+
     return (
-        <>
-            <h3>Login</h3>
-            <span>ID</span>
-            <input type="text" placeholder="아이디를 입력해주세요" />
-            <span>PASSWORD</span>
-            <input type="text" placeholder="비밀번호를 입력해주세요" />
-            <button type="button">로그인</button>
-            <button type="button">회원가입</button>
-        </>
+        <S.Container>
+            <S.InputContainer>
+                <S.PageTitle>Login</S.PageTitle>
+                <S.Field className='id-field'>ID</S.Field>
+                <input
+                    type="text"
+                    size="25"
+                    placeholder="아이디를 입력해주세요"
+                    value={username}
+                    onChange={saveUsername} />
+                <S.Field className='pw-field'>PASSWORD</S.Field>
+                <input
+                    type="text"
+                    size="25"
+                    placeholder="비밀번호를 입력해주세요"
+                    value={password}
+                    onChange={savePassword} />
+            </S.InputContainer>
+            <S.ButtonContainer>
+                <S.Button type="button">로그인</S.Button>
+                <S.SignUpBtn type="button" onClick={moveSignupPage}>회원가입</S.SignUpBtn>
+            </S.ButtonContainer>
+        </S.Container>
     );
 };
 
